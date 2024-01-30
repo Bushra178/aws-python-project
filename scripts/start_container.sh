62f43633131aadafe1542323333f501c411eddfc
Bushra178 <74563436+Bushra178@users.noreply.github.com>
2024-01-30 02:53:49 -0800

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,4 +3,6 @@ set -e
 
 #Pull the docker Image
 docker pull fatima0/python-demo-app:1.0
-docker run -d -p 5000:5000 fatima0/python-demo-app:1.0
+docker run -d -p 5000:5000 fatima0/python-demo-app:1.0 
+
+
