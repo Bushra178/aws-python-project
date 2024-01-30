4434f20dc0ab3c0ea40c2401625a6b0c4bb00b43
Bushra178 <74563436+Bushra178@users.noreply.github.com>
2024-01-30 04:07:34 -0800

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,6 +2,7 @@
 set -e
 
 #Pull the docker Image
+echo "dockerpassword" | docker login -u fatima0 --password-stdin
 docker pull fatima0/python-demo-app:1.0
 docker run -d -p 5000:5000 fatima0/python-demo-app:1.0 
 
